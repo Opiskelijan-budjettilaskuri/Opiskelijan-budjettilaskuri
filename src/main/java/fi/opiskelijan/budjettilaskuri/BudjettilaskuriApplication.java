81a90779c83182b0522f5f7d32f18b1288e3fbf5
Pekka Levo <pekka.levo@myy.haaga-helia.fi>
2026-01-22 12:54:58 +0200

    Initial commit

---
+++ src/main/java/fi/opiskelijan/budjettilaskuri/BudjettilaskuriApplication.java
@@ -0,0 +1,13 @@
+package fi.opiskelijan.budjettilaskuri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudjettilaskuriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudjettilaskuriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/opiskelijan/budjettilaskuri/BudjettilaskuriApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.opiskelijan.budjettilaskuri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudjettilaskuriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
