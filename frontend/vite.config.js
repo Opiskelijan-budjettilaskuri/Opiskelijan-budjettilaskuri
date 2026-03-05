4639bb76115d32503fee0c9f6d366f274faec9c4
Pekka Levo <pekka.levo@myy.haaga-helia.fi>
2026-03-05 13:22:29 +0200

    Fix: korjattu base takaisin repo-name sisältöön

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   //highlight-next-line
-  base: '/',
+  base: '/{repo-name}/',
   plugins: [react()],
   test: {
     globals: true,
