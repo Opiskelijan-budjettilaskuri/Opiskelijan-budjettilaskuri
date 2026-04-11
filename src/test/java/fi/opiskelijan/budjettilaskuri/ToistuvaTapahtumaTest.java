5b5a083e51aa1563e8e78fa47667be93572be09a
Pekka Levo <pekka.levo@myy.haaga-helia.fi>
2026-04-11 14:40:02 +0300

    Korjaus: korjattu virheelliset yksikkötestit ToistuvaTapahtumalle ja Kategorialle

--- src/test/java/fi/opiskelijan/budjettilaskuri/ToistuvaTapahtumaTest.java
+++ src/test/java/fi/opiskelijan/budjettilaskuri/ToistuvaTapahtumaTest.java
@@ -3,6 +3,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import fi.opiskelijan.budjettilaskuri.domain.ToistuvaTapahtuma;
 import fi.opiskelijan.budjettilaskuri.domain.Kategoria;
+import java.time.LocalDate;
 public class ToistuvaTapahtumaTest {
 @Test
 void testGetId() {
@@ -32,26 +33,26 @@ assertEquals("Päivittäinen", perunaruoka.getToistuvuus());
 @Test
 void testGetAloitusPvm() {
 var urheiluvaatteet = new ToistuvaTapahtuma(6, "Verkkarien ostaminen", 17.01, "Urheiluvaatteet", "Viikottainen", 2005, 1, 6, 2005, 11, 6, 2005, 6, 6, false, 2);
-assertEquals(2005, 1, 6 , urheiluvaatteet.getAloitusPvm());
+assertEquals(LocalDate.of(2005, 1, 6), urheiluvaatteet.getAloitusPvm());
 }
 @Test
 void testGetLopetusPvm() {
 var some = new ToistuvaTapahtuma(7, "YouTube-elokuvan vuokraaminen", 18.01, "Sosiaalisessa mediassa tapahtuva vuokraaminen", "Kuukausittainen", 2006, 1, 7, 2006, 11, 7, 2006, 6, 7, true, 3);
-assertEquals(2006, 11, 7, some.getLopetusPvm());
+assertEquals(LocalDate.of(2006, 11, 7), some.getLopetusPvm());
 }
 @Test
 void testGetViimeksiLuotuPvm() {
 var viihde = new ToistuvaTapahtuma(8, "Videopelin ostaminen", 19.01, "Viihde", "Vuosittainen", 2007, 1, 8, 2007, 11, 8, 2007, 6, 8, false, 4);
-assertEquals(2007, 6, 8, viihde.getViimeksiLuotuPvm());
+assertEquals(LocalDate.of(2007, 6, 8), viihde.getViimeksiLuotuPvm());
 }
 @Test
 void testGetAktiivinen() {
 var riisiruoka = new ToistuvaTapahtuma(9, "Chili con carnen aineksien ostaminen", 20.01, "Riisiruoka", "Päivittäinen", 2008, 1, 9, 2008, 11, 9, 2008, 6, 9, true, 1);
-assertEquals(false, riisiruoka.getAktiivinen());
+assertEquals(true, riisiruoka.getAktiivinen());
 }
 @Test
 void testGetKategoria() {
 var alusvaatteet = new ToistuvaTapahtuma(10, "Alushousujen ostaminen", 21.11, "Alusvaatteet", "Viikottainen", 2009, 1, 10, 2009, 11, 10, 2009, 6, 10, false, 2);
-assertEquals(2, alusvaatteet.getAktiivinen());
+assertEquals(2, alusvaatteet.getKategoria().getId());
 }
 }
