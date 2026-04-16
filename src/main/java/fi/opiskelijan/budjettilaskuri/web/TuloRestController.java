0e834918785bc2d3495e8f7d457952aaa8243f3a
Pekka Levo <pekka.levo@myy.haaga-helia.fi>
2026-04-16 12:21:39 +0300

    Lisäys: @CrossOrigin tulorestcontrolleriin

--- src/main/java/fi/opiskelijan/budjettilaskuri/web/TuloRestController.java
+++ src/main/java/fi/opiskelijan/budjettilaskuri/web/TuloRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import fi.opiskelijan.budjettilaskuri.domain.Tulo;
 import fi.opiskelijan.budjettilaskuri.repository.TuloRepository;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/tulot")
 public class TuloRestController {
