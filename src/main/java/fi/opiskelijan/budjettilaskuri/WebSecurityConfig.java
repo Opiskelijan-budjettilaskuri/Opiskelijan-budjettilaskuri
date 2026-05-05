882cd7c37a1b15e0287a2bbcfaec51899d7fae34
robbiewinter <robbie.winter@myy.haaga-helia.fi>
2026-05-05 10:45:56 +0300

    Korjaa uloskirjautuminen

--- src/main/java/fi/opiskelijan/budjettilaskuri/WebSecurityConfig.java
+++ src/main/java/fi/opiskelijan/budjettilaskuri/WebSecurityConfig.java
@@ -2,10 +2,8 @@ package fi.opiskelijan.budjettilaskuri;
 
 import java.util.Arrays;
 
-import org.springframework.boot.actuate.autoconfigure.observation.ObservationProperties.Http;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -55,6 +53,7 @@ public class WebSecurityConfig {
                     "/kirjaudu",
                     "/rekisteroidy",
                     "/tallennakayttaja",
+                    "/api/ulosKirjautuminen",
                     "/js/**",
                     "/images/**"
                 ).permitAll()
--- src/main/java/fi/opiskelijan/budjettilaskuri/web/KayttajaController.java
+++ src/main/java/fi/opiskelijan/budjettilaskuri/web/KayttajaController.java
@@ -1,6 +1,10 @@
 package fi.opiskelijan.budjettilaskuri.web;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import fi.opiskelijan.budjettilaskuri.domain.Kayttaja;
 import fi.opiskelijan.budjettilaskuri.repository.KayttajaRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class KayttajaController {
@@ -38,5 +44,14 @@ public class KayttajaController {
         repository.save(kayttaja);
 
         return "redirect:/kirjaudu";
+    }
+
+    @PostMapping("/api/ulosKirjautuminen")
+    public ResponseEntity<?> logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return ResponseEntity.ok().build();
     }
 }
